7a7e89f73ed387640a75eeb19dbe2d360e663d2d
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2021-05-11 12:08:12 -0700

    simple tools to find and save the max a priori sol

---
+++ utils.py
@@ -0,0 +1,124 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import starry
+from copy import deepcopy
+import astropy.units as u
+import astropy.constants as const
+from astropy.io import fits, ascii
+from astropy.table import Table
+from corner import corner
+import pymc3 as pm
+from celerite2.theano import terms, GaussianProcess
+import pymc3_ext as pmx
+import os
+
+starry.config.lazy = True
+starry.config.quiet = True
+
+class starry_basemodel():
+    def __init__(self,dataPath='sim_data/sim_data_baseline.ecsv',
+                 descrip='Orig_001'):
+        """
+        Set up a starry model
+        
+        """
+        self.data_path = dataPath
+        self.get_data(path=self.data_path)
+        self.mask = None
+        self.descrip = descrip
+        self.mxap_name = 'mxap_{}.npz'.format(self.descrip)
+        self.mxap_path = os.path.join('fit_data','mxap_soln',self.mxap_name)
+    
+    def get_data(self,path):
+        """ Gather the data
+        """
+        self.dat = ascii.read(path)
+        #self.tref = np.round(np.min(self.dat['Time (days)']))
+        self.x = np.ascontiguousarray(self.dat['Time (days)'])# - self.tref)
+        self.y = np.ascontiguousarray(self.dat['Flux'])
+        self.yerr = np.ascontiguousarray(self.dat['Flux err'])
+        self.meta = self.dat.meta
+    
+    def build_model(self):
+        if self.mask is None:
+            self.mask = np.ones(len(self.x), dtype=bool)
+        
+        with pm.Model() as model:
+            
+            A_map = starry.Map(ydeg=0,udeg=2,amp=1.0)
+            A = starry.Primary(A_map,m=self.meta['M_star'],
+                               r=self.meta['R_star'],
+                               prot=self.meta['prot_star'])
+            
+            b_map = starry.Map(ydeg=3)
+            b_map.amp = 1e-3
+            
+            sec_mu = np.zeros(b_map.Ny)
+            sec_mu[0] = 1e-3
+            sec_L = np.zeros(b_map.Ny)
+            sec_L[0] = (0.2 * sec_mu[0])**2 ## covariance is squared
+            sec_L[1:] = (0.5 * sec_mu[0])**2
+            
+            b_map.set_prior(mu=sec_mu, L=sec_L)
+            b = starry.kepler.Secondary(b_map,
+                                        m=0.0,
+                                        r=self.meta['rp'],
+                                        prot=self.meta['Period'],
+                                        porb=self.meta['Period'],
+                                        t0=self.meta['t0'],
+                                        inc=self.meta['inc'])
+            b.theta0 = 180.0
+            sys = starry.System(A,b)
+            
+            self.sys = sys
+            
+            lc_eval = pm.Deterministic('lc_eval',sys.flux(t=self.x[self.mask]))
+            resid = self.y[self.mask] - lc_eval
+            
+            ## estimate the standard deviation
+            sigma_lc = pm.Lognormal("sigma_lc", mu=np.log(np.std(self.y[self.mask])), sigma=0.5)
+            
+            ## estimate GP error as std
+            sigma_gp = pm.Lognormal("sigma_gp", mu=np.log(np.std(self.y[self.mask]) * 1.0), sigma=0.5)
+            rho_gp = pm.Lognormal("rho_gp", mu=np.log(5e-2), sigma=0.5)
+            #tau_gp = pm.Lognormal("tau_gp",mu=np.log(5e-2), sigma=0.5)
+            #kernel = terms.SHOTerm(sigma=sigma_gp, rho=rho_gp, tau=tau_gp)
+            
+            ## non-periodic
+            kernel = terms.SHOTerm(sigma=sigma_gp, rho=rho_gp, Q=0.25)
+            
+            gp = GaussianProcess(kernel, t=self.x[self.mask], yerr=sigma_lc,quiet=True)
+            gp.marginal("gp", observed=resid)
+            #gp_pred = pm.Deterministic("gp_pred", gp.predict(resid))
+            final_lc = pm.Deterministic("final_lc",lc_eval + gp.predict(resid))
+            
+            # Save our initial guess w/ just the astrophysical component
+            self.lc_guess_astroph = pmx.eval_in_model(lc_eval)
+            self.lc_guess = pmx.eval_in_model(final_lc)
+            self.resid_guess = pmx.eval_in_model(resid)
+        self.model = model
+    
+    def plot_lc(self,point='guess'):
+        if hasattr(self,'lc_guess') == False:
+            self.build_model()
+        #f =  self.sys.flux(self.x).eval()
+        f = self.lc_guess
+        plt.plot(self.x,self.y,'.',label='data')
+        plt.plot(self.x,f,label='GP model')
+        plt.plot(self.x,self.lc_guess_astroph,label='Astrophysical Component')
+        plt.legend()
+        plt.show()
+        
+    def find_mxap(self):
+        if hasattr(self,'model') == False:
+            self.build_model()
+        
+        with self.model:
+            self.mxap_soln =pmx.optimize()
+        
+        np.savez(self.mxap_path,**self.mxap_soln)
+        
+    def read_mxap(self):
+        npzfiles  = np.load(self.mxap_path)#,allow_pickle=True)
+        self.mxap_soln = dict(npzfiles)
+    
