7049d4e3eecd77895935a5e6598c4e1795507100
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2022-06-06 17:27:38 -0400

    made a single function to fit both planets with an astrophysical-only model to lightcuves with baseline trends

--- run_example_fits.py
+++ run_example_fits.py
@@ -17,11 +17,14 @@ def zero_eclipse_GP(descrip="Orig_006_newrho_smallGP"):
     sb.plot_lc(point='mxap')
 
 
-def make_plots_for_no_gp_fit_for_paper_hd189():
-    sb0, sb1 = make_sb_obj_with_no_gp(degree=2)
-    sb1.run_all()
-    sb0_with_gp, sb1_with_gp = make_sb_objects(degree=2,use_gp_list=[False, True])
-    utils.compare_histos(sb1,sb1_with_gp,dataDescrips=['No GP, planet only','GP for systematics'])
+def make_plots_for_no_gp_fit_for_paper():
+    for lc_name, degree in zip(['orig','NC_HD189'],[3,2]):
+        sb0, sb1 = make_sb_obj_with_no_gp(degree=degree,lc_name=lc_name)
+        sb1.run_all()
+        sb0_with_gp, sb1_with_gp = make_sb_objects(degree=degree,use_gp_list=[False, True],
+                                                   lc_name=lc_name)
+        utils.compare_histos(sb1,sb1_with_gp,dataDescrips=['No GP, planet only','GP for systematics'])
+    
 
 def make_sb_obj_with_no_gp(map_type='variable',lc_name='NC_HD189',
                            map_prior='physical',degree=2,
